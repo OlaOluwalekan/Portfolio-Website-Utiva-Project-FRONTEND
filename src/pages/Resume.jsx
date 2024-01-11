69d689f62bc699db8855d89564ee683ef911bab2
OlaOluwalekan <81148417+OlaOluwalekan@users.noreply.github.com>
2024-01-11 13:06:43 +0100

    Update Resume.jsx
    Updated cloudinary link to resume in Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,7 +2,7 @@ import styles from '../css/resume.module.css'
 
 const Resume = () => {
   const resumeLink =
-    'https://res.cloudinary.com/dyyoorpns/image/upload/v1695117245/Bells%20Arena/static%20assets/pdf/resume_19092023.pdf'
+    'https://res.cloudinary.com/dyyoorpns/image/upload/v1704974522/Bells%20Arena/static%20assets/pdf/resume_19092023.pdf'
 
   return (
     <div className={styles.main}>
